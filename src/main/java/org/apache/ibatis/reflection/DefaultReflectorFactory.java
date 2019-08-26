596f39bd29b3be91953a21c36b5670d333951b26
daiwenjun001 <570459276@qq.com>
2019-08-26 16:14:17 +0800

    mybatis源码

---
+++ src/main/java/org/apache/ibatis/reflection/DefaultReflectorFactory.java
@@ -0,0 +1,61 @@
+/**
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.reflection;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * 默认的 ReflectorFactory 实现类
+ */
+public class DefaultReflectorFactory implements ReflectorFactory {
+  /**
+   * 是否缓存,默认为 true 缓存
+   */
+  private boolean classCacheEnabled = true;
+  /**
+   * Reflector 的缓存映射
+   * key : 类
+   * value : Reflector 对象
+   */
+  private final ConcurrentMap<Class<?>, Reflector> reflectorMap = new ConcurrentHashMap<>();
+
+  public DefaultReflectorFactory() {
+  }
+
+  @Override
+  public boolean isClassCacheEnabled() {
+    return classCacheEnabled;
+  }
+
+  @Override
+  public void setClassCacheEnabled(boolean classCacheEnabled) {
+    this.classCacheEnabled = classCacheEnabled;
+  }
+
+  @Override
+  public Reflector findForClass(Class<?> type) {
+    //如果开启了缓存,则从 reflectorMap 中取
+    if (classCacheEnabled) {
+      // synchronized (type) removed see issue #461
+      return reflectorMap.computeIfAbsent(type, Reflector::new);
+    } else {
+      //未开启缓存,直接返回新创建的对象
+      return new Reflector(type);
+    }
+  }
+
+}
